6f15092c67a5e0c03c29cf639a77c1dc70df2d2a
Max Zakzook <mzakzook@me.com>
2020-04-14 18:56:38 -0700

    Fix edge cases

--- primeNumberDecomp.js
+++ primeNumberDecomp.js
@@ -1,7 +1,9 @@
 function getAllPrimeFactors(n) { 
   let results = []
+  if (n === 1) return [1]
   let i = 2
-  while (n > 1) {
+  let sqrt = Math.floor(Math.sqrt(n))
+  while (n > 1 && i <= sqrt) {
     if (n % i === 0) {
       results.push(i)
       n /= i
@@ -9,6 +11,7 @@ function getAllPrimeFactors(n) {
       i++
     }
   }
+  if (n > 1 && Math.floor(n) === n) results.push(n)
   return results
 }
 
@@ -16,6 +19,7 @@ function getAllPrimeFactors(n) {
 function getUniquePrimeFactorsWithCount(n) { 
   let preResults = getAllPrimeFactors(n)
   let results = [[],[]]
+  if (preResults.length === 0) return results
   let count = 1
   for (let i = 0; i < preResults.length - 1; i++) {
     if (preResults[i] === preResults[i + 1]) {
@@ -39,8 +43,44 @@ function getUniquePrimeFactorsWithCount(n) {
 function getUniquePrimeFactorsWithProducts(n) { 
   let preResults = getUniquePrimeFactorsWithCount(n)
   let results = []
+  if (preResults[0].length === 0) return results
   for (let i = 0; i < preResults[0].length ; i++) {
     results.push(preResults[0][i] ** preResults[1][i])
   }
   return results
 }
+
+
+
+console.log(getAllPrimeFactors(100)) //[2,2,5,5]
+
+console.log(getUniquePrimeFactorsWithCount(100)) //[[2,5],[2,2]]
+
+console.log(getUniquePrimeFactorsWithProducts(100)) //[4,25]
+
+
+
+console.log(getAllPrimeFactors(2)) 
+
+console.log(getUniquePrimeFactorsWithCount(2))
+
+console.log(getUniquePrimeFactorsWithProducts(2)) 
+
+
+
+console.log(getAllPrimeFactors(1)) 
+
+console.log(getUniquePrimeFactorsWithCount(1))
+
+console.log(getUniquePrimeFactorsWithProducts(1)) 
+
+
+
+
+console.log(getUniquePrimeFactorsWithCount(113))
+
+
+console.log(getUniquePrimeFactorsWithCount(114))
+
+
+console.log(getUniquePrimeFactorsWithCount(115))
