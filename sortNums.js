1ee5477d9afb00803b4d767903fb0d55c116bd0d
Max Zakzook <mzakzook@me.com>
2020-04-17 16:01:07 -0700

    Sort number words

---
+++ sortNums.js
@@ -0,0 +1,66 @@
+// Hey You !
+// Sort these integers for me ...
+
+// By name ...
+
+// Do it now !
+
+// Input
+// Range is 0-999
+
+// There may be duplicates
+
+// The array may be empty
+
+// Example
+// Input: 1, 2, 3, 4
+// Equivalent names: "one", "two", "three", "four"
+// Sorted by name: "four", "one", "three", "two"
+// Output: 4, 1, 3, 2
+// Notes
+// Don't fret about formatting rules. If rules are consistently applied it has no effect anyway:
+// e.g. one hundred one, one hundred two is same order as one hundred and one, one hundred and two
+// e.g. ninety eight, ninety nine is same order as ninety-eight, ninety-nine
+
+
+function sortByName(ary) {
+  let s = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  let t = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let z = ['hundred']
+  let resObj = {}
+  let wordArr = []
+
+  let numArr = ary.forEach(num => {
+    let resStr = ''
+    let numStr = num.toString()
+    if (numStr.length === 1) {
+      resStr = s[num] 
+    }
+    if (numStr.length === 2) {
+      if (numStr[0] === "1") {
+        resStr = s[num]
+      } else {
+        resStr += t[numStr[0] - 1]
+        if (numStr[1] > 0) resStr += s[numStr[1]]
+      }
+    }
+    if (numStr.length === 3) {
+      resStr += s[numStr[0]] + ' hundred'
+      if (numStr[1] === '1') {
+        resStr += ' ' + s[numStr.slice(1)]
+      } else if (numStr[1] === '0' && numStr[2] !== '0') {
+        resStr += ' ' + s[numStr[2]]
+      } else if (numStr[1] !== '0') {
+        resStr += ' ' + t[numStr[1] - 1]
+        if (numStr[2] !== '0') {
+          resStr += ' ' + s[numStr[2]]
+        }
+      }
+    }
+    if (!resObj[resStr]) resObj[resStr] = num
+    wordArr.push(resStr)
+  }) 
+  return wordArr.sort().map(num => resObj[num])
+}
+
+
