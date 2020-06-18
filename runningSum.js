75e26a771c401f25872ffe82933b52aff869bb95
Max Zakzook <mzakzook@me.com>
2020-06-18 16:55:39 -0700

    Running sum

---
+++ runningSum.js
@@ -0,0 +1,43 @@
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+// Example 2:
+
+// Input: nums = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+// Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+// Example 3:
+
+// Input: nums = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 1000
+// -10^6 <= nums[i] <= 10^6
+
+
+
+var runningSum = function(nums) {
+  if (nums.length === 1) return nums[0];
+  let result = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+      result.push(nums.slice(0, i + 1).reduce((a,b) => a + b, 0));
+  }
+  return result;
+};
+
+
+
+console.log(runningSum([1,2,3,4])); // -> [1,3,6,10]
+console.log(runningSum([1,1,1,1,1])); // -> [1,2,3,4,5]
+console.log(runningSum([3,1,2,10,1])); // -> [3,4,6,16,17]
