fbfc7885ff5833199105392df734922550f052a5
Max Zakzook <mzakzook@me.com>
2020-08-28 18:33:23 -0700

    Minimum Operations to make Array Equal

--- increaseDecreaseString.js
+++ increaseDecreaseString.js
@@ -47,31 +47,31 @@
 // s contains only lower-case English letters.
 
 var sortString = function(s) {
-    let sortedS = s.split('').sort();
-    let result = '';
-    while (sortedS.length > 0) {
-        result += sortedS[0];
-        sortedS.splice(0, 1);
-        let copyS = JSON.parse(JSON.stringify(sortedS));
-        for (let i = 0; i < sortedS.length; i++) {
-            if (sortedS[i] !== result.slice(-1)) {
-                result += sortedS[i];
-                copyS.splice(copyS.indexOf(sortedS[i]), 1);
-            }
-        }
-        if (copyS.length > 0) result += copyS.slice(-1);
-        copyS.pop();
-        sortedS = JSON.parse(JSON.stringify(copyS));
-        for (let i = sortedS.length - 1; i >= 0; i--) {
-            if (sortedS[i] !== result.slice(-1)) {
-                result += sortedS[i];
-                copyS.splice(i, 1);
-            }
-        }
-        sortedS = JSON.parse(JSON.stringify(copyS));
-    }
-    return result;
-  };
+  let sortedS = s.split('').sort().join('');
+  let result = '';
+  while (sortedS.length > 0) {
+      result += sortedS[0];
+      sortedS.splice(0, 1);
+      let copyS = sortedS;
+      for (let i = 0; i < sortedS.length; i++) {
+          if (sortedS[i] !== result.slice(-1)) {
+              result += sortedS[i];
+              copy.splice(i, 1);
+          }
+      }
+      result += copy.slice(-1);
+      copy.pop();
+      sortedS = copy;
+      for (let i = sortedS.length - 1; i >= 0; i--) {
+          if (sortedS[i] !== result.slice(-1)) {
+              result += sortedS[i];
+              copy.splice(i, 1);
+          }
+      }
+      sortedS = copy;
+  }
+  return result;
+};
 
 
 console.log(sortString("aaaabbbbcccc")); // => "abccbaabccba"
